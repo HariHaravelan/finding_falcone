f47c560cad5c48110008abb39f5d53f1604b1535
hnarayanan <hariharavelan.narayanan@redflex.com>
2020-12-16 17:06:53 +0530

    Add working page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Typography from "@material-ui/core/Typography";
+import Grid from '@material-ui/core/Grid';
+import {fetchPlanets} from "./services/PlanetService";
+import {fetchVehicles} from "./services/VehicleService";
+import JourneyPlan from "./components/JourneyPlan";
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [planets, setPlanets] = useState([]);
+    const [vehicles, setVehicles] = useState([]);
+    const [availableVehicles, setAvailableVehicles] = useState([]);
+    const [availablePlanets, setAvailablePlanets] = useState([]);
+    const [selectedPlanets, setSelectedPlanets] = useState([]);
+    const [selectedVehicles, setSelectedVehicles] = useState([]);
 
+    useEffect(() => {
+        fetchPlanets().then(({data}) => {
+            setAvailablePlanets(data);
+            setPlanets(data);
+        }).catch(() => {
+            setAvailablePlanets([]);
+            setPlanets([])
+        });
+        fetchVehicles().then(({data}) => {
+            setVehicles(data);
+            setAvailableVehicles(data);
+        }).catch(() => {
+            setVehicles([]);
+            setAvailableVehicles([])
+        });
+    }, []);
+
+
+    const addSelectedVehicles = (event) => {
+        const vehicles = [...selectedVehicles];
+        vehicles.push(event.target.value);
+        setSelectedVehicles(vehicles);
+    };
+
+    const addSelectedPlanets = (event) => {
+        const planets = [...selectedPlanets];
+        planets.push(event.target.value);
+        setSelectedPlanets(planets);
+        setAvailablePlanets(availablePlanets.filter((planet) => planet.name !== event.target.innerHTML));
+    };
+
+    return (
+        <div className="App">
+            <Typography variant="h3" align={"center"}>
+                Finding Falcone!
+            </Typography>
+
+            <Grid container>
+                {_.times(4, (index) => {
+                    return (
+                        <Grid item xs={6} sm={3} key={index}>
+                            <JourneyPlan index={index} planets={availablePlanets} vehicles={availableVehicles}
+                                         onDestinationChange={addSelectedPlanets}
+                                         onVehicleChange={addSelectedVehicles}
+                            />
+                        </Grid>);
+                })}
+            </Grid>
+        </div>);
+};
 export default App;
